f9310f588819b0226b233a9756d1721a0e8ebb13
SIARHEI SAVERCHANKA <lordofdarkness011@tut.by>
2020-10-18 17:04:17 +0300

    firstTry

---
+++ src/bsu/rfe/java/group10/lab1/Saverchenko/varC/MainApplication.java
@@ -0,0 +1,8 @@
+package bsu.rfe.java.group10.lab1.Saverchenko.varC;
+
+public class MainApplication {
+    public static void main(String[] args)
+    {
+        System.out.print("Hello World!");
+    }
+}
